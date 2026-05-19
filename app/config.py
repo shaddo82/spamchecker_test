f376e288f049c7bef80251277b84fcfe73265cf6
황재민 <ghkd1791@gmail.com>
2026-05-19 13:56:45 +0900

    mlflow

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 import os
-
 MODEL_MODE = "ml" # "rules"
-# LOCAL_MODEL_PATH = "ml/artifacts/spam_model.joblib"
 MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
-# MODEL_URI = "runs:/ff834da846d94fdc858c14d00aa89064/model"
-# MODEL_URI = "models:/spam-model/1"
 MODEL_URI = "models:/spam-model@champion"
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+MODEL_NAME = "spam_model.joblib"
+ARTIFACT_DIR_NAME = "artifacts"
+DATA_DIR_NAME = "data"
--- ml/train.py
+++ ml/train.py
@@ -4,6 +4,8 @@ import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score # 성능 지표 저장을 위해
 import mlflow.sklearn # mlflow 형태로 저장
 
@@ -13,38 +15,55 @@ ARTIFACT_DIR = os.path.join(BASE_DIR, "artifacts")
 MODEL_PATH = os.path.join(ARTIFACT_DIR, "spam_model.joblib")
 os.makedirs(ARTIFACT_DIR, exist_ok=True)
 # 실험 세팅
+from app.config import *
+BASE_DIR = os.path.dirname(__file__)
+TRAIN_DATA_PATH = os.path.join(BASE_DIR, DATA_DIR_NAME,TRAIN_FILE_NAME)
+TEST_DATA_PATH = os.path.join(BASE_DIR, DATA_DIR_NAME, TEST_FILE_NAME)
+ARTIFACT_DIR = os.path.join(BASE_DIR, ARTIFACT_DIR_NAME)
 mlflow.set_tracking_uri("sqlite:///mlflow.db") # mlflow는 sqlite를 기본 사용
 mlflow.set_experiment("spam-classification-local")
-df = pd.read_csv(DATA_PATH)
-X = df["text"]
-y = df["label"]
-pipeline = Pipeline([
-    ("vectorizer", CountVectorizer()),
-    ("classifier", LogisticRegression(max_iter=200))
-])
+train_df = pd.read_csv(TRAIN_DATA_PATH)
+test_df = pd.read_csv(TEST_DATA_PATH)
+X_train = train_df["text"]
+y_train = train_df["label"]
+X_test = test_df["text"]
+y_test = test_df["label"]
+models = {
+"LogisticRegression": LogisticRegression(max_iter=200),
+"NaiveBayes": MultinomialNB(),
+"RandomForestClassifier": RandomForestClassifier(n_estimators=100, random_state=42)
+}
 # 실험 기록 시작
-with mlflow.start_run():
-    # 실험 설정 기록
-    mlflow.log_param("model_type"
-    ,
-    "LogisticRegression")
-    mlflow.log_param("vectorizer",
-    "CountVectorizer")
-    mlflow.log_param("max_iter"
-    , 200)
-    mlflow.log_param("data_path", DATA_PATH)
-    mlflow.log_param("row_count"
-    , len(df))
-    pipeline.fit(X, y)
-    # 간단한 metric 저장 (train accuracy)
-    preds = pipeline.predict(X)
-    acc = accuracy_score(y, preds)
-    mlflow.log_metric("train_accuracy", acc)
-    joblib.dump(pipeline, MODEL_PATH)
-    # artifact 기록
-    mlflow.log_artifact(DATA_PATH) # 데이터 = 실무에서는 데이터는 따로 관리
-    mlflow.log_artifact(MODEL_PATH) # 모델 파일
-    # MLflow 모델 형식으로도 저장
-    mlflow.sklearn.log_model(pipeline, name="model", registered_model_name="spam-model")
-    print(f"Model saved to: {MODEL_PATH}")
-    print(f"train_accuracy: {acc:.4f}")
+for model_name, model in models.items():
+    with mlflow.start_run():
+        # 실험 설정 기록
+        pipeline = Pipeline([
+            ("vectorizer", CountVectorizer()),
+            ("classifier", model)
+        ])
+        mlflow.log_param("train_data_path", TRAIN_DATA_PATH)
+        mlflow.log_param("test_train_data_path", TEST_DATA_PATH)
+        mlflow.log_param("train_row_count"
+                         , len(train_df))
+        mlflow.log_param("test_row_count"
+                         , len(test_df))
+        pipeline.fit(X_train, y_train)
+        # 간단한 metric 저장 (train accuracy)
+        train_preds = pipeline.predict(X_train)
+        test_preds = pipeline.predict(X_test)
+        train_acc = accuracy_score(y_train, train_preds)
+        test_acc = accuracy_score(y_test, test_preds)
+        mlflow.log_metric("train_accuracy"
+                          , train_acc)
+        mlflow.log_metric("test_accuracy"
+                          , test_acc)
+        joblib.dump(pipeline, MODEL_PATH)
+        # artifact 기록
+        mlflow.log_artifact(TRAIN_DATA_PATH)  # 데이터
+        mlflow.log_artifact(TEST_DATA_PATH)  # 데이터
+        mlflow.log_artifact(MODEL_PATH)  # 모델 파일
+        # MLflow 모델 형식으로도 저장
+        mlflow.sklearn.log_model(pipeline, name="model", registered_model_name="spam-model")
+        print(f"Model saved to: {MODEL_PATH}")
+        print(f"train_accuracy: {train_acc:.4f}")
+        print(f"test_accuracy: {test_acc:.4f}")
